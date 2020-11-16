9fda9014cb6c163132933f6942d8e353e03d06d5
Krzysztof Isbrandt <kris.isbrandt@gmail.com>
2020-11-16 21:48:25 +0100

    Board implementation

--- src/main/java/com/example/chess/board/BoardUtils.java
+++ src/main/java/com/example/chess/board/BoardUtils.java
@@ -1,5 +1,9 @@
 package com.example.chess.board;
 
+/**
+ * BoardUtils class provides only static fields and columns
+ * the constructor is private and even if it is called it will throw RunTimeException
+ */
 public class BoardUtils {
 
     public static final boolean[] FIRST_COLUMN = initColumn(0);
@@ -7,8 +11,8 @@ public class BoardUtils {
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
 
-    public static final boolean[] SECOND_ROW = null;
-    public static final boolean[] SEVENTH_ROW = null;
+    public static final boolean[] SECOND_ROW = initRow(8);
+    public static final boolean[] SEVENTH_ROW = initRow(48);
 
     public static final int NUM_TILES = 64;
     public static final int NUM_TILES_PER_ROW = 8;
@@ -24,6 +28,15 @@ public class BoardUtils {
             columnNumber += NUM_TILES_PER_ROW;
         } while (columnNumber < NUM_TILES);
         return column;
+    }
+
+    private static boolean[] initRow(int rowNumber) {
+        final boolean[] row = new boolean[NUM_TILES];
+        do {
+            row[rowNumber] = true;
+            rowNumber++;
+        } while (rowNumber % NUM_TILES_PER_ROW != 0);
+        return row;
     }
 
     public static boolean isValidCoordinate(final int coordinate) {
